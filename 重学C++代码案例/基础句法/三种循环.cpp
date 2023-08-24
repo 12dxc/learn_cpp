184e6768a7dc59ef920430762a81e0511e865c3a
12dxc <108205955+12dxc@users.noreply.github.com>
2023-08-24 13:16:15 +0800

    Add files via upload

---
+++ 重学C++代码案例/基础句法/三种循环.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+//三种循环for、while、do...while
+int main()
+{
+    //TODO：1+2+3+4...+100
+    //while语句
+    int sum = 0;
+    int index = 1;
+    while (index <= 100)
+    {
+        sum += index;
+        index++;
+    }
+    //cout << sum << endl;
+
+    //for循环   
+    sum = 0;
+    index = 1;
+    for (; index <= 100; index++)
+    {
+        sum += index;
+    }
+    //cout << sum << endl;
+
+    sum = 0;
+    index = 1;
+    //do...while语句
+    do
+    {
+        sum += index;
+        index++;
+    } while (index <= 100);
+    //cout << sum << endl;
+
+    system("pause");
+    return 0;
+}
---
+++ 重学C++代码案例/基础句法/枚举.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    enum wT { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday };
+    wT weekday;
+    weekday = Monday;
+    weekday = Tuesday;
+    //weekday = 1;   //不能直接给int值，只能赋值成wT定义好的类型值
+    cout << weekday << endl;
+    //Monday = 0;    //类型值不能做左值
+    int a = Wednesday;    //枚举变量可直接赋给非枚举变量
+    cout << a << endl;
+
+    system("pause");
+    return 0;
+}
