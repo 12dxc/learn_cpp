6ebd9f4691027fe89095381215de730e2d4c8ffc
12dxc <108205955+12dxc@users.noreply.github.com>
2024-02-09 00:30:39 +0800

    Add files via upload

---
+++ linux高并发服务器编程/linux网络编程/代码/epoll反应堆和线程池_UDP/udp_server.c
@@ -0,0 +1,46 @@
+﻿#include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+int main()
+{
+    // 1、创建套接字
+    int fd = socket(AF_INET, SOCK_DGRAM, 0);
+    // 2、绑定
+    struct sockaddr_in myaddr;
+    myaddr.sin_family = AF_INET;
+    myaddr.sin_port = htons(8080);
+    myaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    int ret = bind(fd, (struct sockaddr *)&myaddr, sizeof(myaddr));
+    if (ret < 0)
+    {
+        perror("");
+        return 0;
+    }
+    // 3、读写
+    char buf[1500] = "";
+    struct sockaddr_in cliaddr;
+    socklen_t len = sizeof(cliaddr);
+    while (1)
+    {
+        memset(buf, 0, sizeof(buf));
+        int n = recvfrom(fd, buf, sizeof(buf), 0, (struct sockaddr *)&cliaddr, &len);
+        if (n < 0)
+        {
+            perror("");
+            break;
+        }
+        else
+        {
+            printf("%s\n", buf);
+            sendto(fd, buf, n, 0, (struct sockaddr *)&cliaddr, len);
+        }
+    }
+    // 4、关闭
+    close(fd);
+
+    return 0;
+}
---
+++ linux高并发服务器编程/linux网络编程/代码/libevent/event_TcpServer.c
@@ -0,0 +1,54 @@
+﻿#include <stdio.h>
+#include "wrap.h"
+#include <event.h>
+
+void *lfdcb(int lfd, short event, void *arg);
+void *cfdcb(int cfd, short event, void *arg);
+
+int main()
+{
+    // 创建套接字  绑定
+    int lfd = tcp4bind(AF_INET, NULL);
+    // 监听
+    Listen(lfd, 128);
+    // 创建event_base根节点
+    struct event_base *base = event_base_new();
+    // 初始化lfd上树节点
+    struct event *ev = event_new(base, lfd, EV_READ | EV_PERSIST, lfdcb, base);
+    // 上树
+    event_add(ev, NULL); // 参2为空，表示永久监听
+    // 循环监听
+    event_base_dispatch(base);
+    // 收尾
+    close(lfd);
+    event_base_free(base);
+
+    return 0;
+}
+
+// 服务器事件
+void *lfdcb(int lfd, short event, void *arg)
+{
+    struct event_base *base = (struct event_base *)arg;
+    // 提取新的cfd
+    int cfd = Accept(lfd, NULL, NULL);
+    // 将cfd上树
+    struct event *ev = event_new(base, cfd, EV_READ | EV_PERSIST, cfdcb, NULL);
+    event_add(ev, NULL);
+}
+// 客户端事件
+void *cfdcb(int cfd, short event, void *arg)
+{
+    char buf[1500] = "";
+    int n = Read(cfd, buf, sizeof(buf));
+    if (n <= 0)
+    {
+        perror("err or close..\n");
+        // 下树
+    }
+    else
+    {
+        printf("%s\n", buf);
+        Write(cfd, buf, n);
+    }
+}
